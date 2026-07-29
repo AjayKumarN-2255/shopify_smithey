c1539bdad36b55832d6dd8dee29b0e6a52024d80
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-07-29 12:12:17 +0530

    mega-menu desk completed

--- assets/header.js
+++ assets/header.js
@@ -42,10 +42,49 @@ document.addEventListener('DOMContentLoaded', () => {
         closeIcon.style.display = 'block';
     });
 
+    // close mobile menu
     closeIcon.addEventListener('click', () => {
         mobileMenu.classList.remove('mobile-menu--open');
 
         closeIcon.style.display = 'none';
         hamburger.style.display = 'block';
+    });
+
+    // open mega menu based on each click
+    const triggers = document.querySelectorAll('[data-menu-trigger]');
+    const megaMenuContainer = document.querySelector('.mega-menu-container');
+    const megaMenus = document.querySelectorAll('.mega-menu');
+
+    triggers.forEach((trigger) => {
+        trigger.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const menuName = trigger.dataset.menuTrigger;
+            const activeMenu = document.querySelector('.mega-menu--active');
+
+            if (
+                megaMenuContainer.classList.contains('mega-menu-container--open') &&
+                activeMenu &&
+                activeMenu.dataset.menu === menuName
+            ) {
+                megaMenuContainer.classList.remove('mega-menu-container--open');
+                header.classList.remove('site-header--mega-open');
+                document.body.classList.remove('no-scroll');
+
+                activeMenu.classList.remove('mega-menu--active');
+                return;
+            }
+
+            megaMenuContainer.classList.add('mega-menu-container--open');
+            header.classList.add('site-header--mega-open');
+            document.body.classList.add('no-scroll');
+
+            megaMenus.forEach((menu) => {
+                menu.classList.toggle(
+                    'mega-menu--active',
+                    menu.dataset.menu === menuName
+                );
+            });
+        });
     });
 });
